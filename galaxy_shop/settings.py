9fe9b840178b4c3ea8e70dc682cf910b5eb71176
bpseitz <72680355+bpseitz@users.noreply.github.com>
2020-11-18 12:59:14 -0800

    added store app
    created store app
    added templates and static files

--- galaxy_shop/settings.py
+++ galaxy_shop/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'store',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
